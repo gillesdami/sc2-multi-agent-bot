8de32189b8f12c27b1b401f9f0bb82699d8e473f
Damien Gilles <pro@gillesdamien.com>
2018-01-10 11:38:44 +0100

    impremented BotStrategy

---
+++ BotStrategy.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <sc2api\sc2_common.h>
+
+using namespace sc2;
+
+struct BotStrategy {
+	double aggressivity;
+	double developpement;
+
+	int enemyMilitarySupply;
+
+	int mineralsAllocated;
+	int vespeneAllocated;
+
+	Point2D** militarySupportRequested;
+	Point2D** militaryObjectives;
+};
