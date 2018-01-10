a9c2b4013a82c3cd48f0e4909f8e40d87940340e
Damien Gilles <pro@gillesdamien.com>
2018-01-10 12:29:13 +0100

    first implementation of SelfObservationInterface, observation is only limited to rawDatas

---
+++ SelfObservationInterface.cpp
@@ -0,0 +1,161 @@
+#include "SelfObservationInterface.h"
+
+
+
+SelfObservationInterface::SelfObservationInterface(Unit* self, ObservationInterface* observations)
+{
+	this->self = self;
+	this->observations = observations;
+}
+
+SelfObservationInterface::~SelfObservationInterface()
+{
+}
+
+uint32_t SelfObservationInterface::GetPlayerID() const {
+	return this->observations->GetPlayerID();
+}
+
+uint32_t SelfObservationInterface::GetGameLoop() const {
+	return this->observations->GetGameLoop();
+}
+
+Units SelfObservationInterface::GetUnits() const {
+	return this->observations->GetUnits();
+}
+
+Units SelfObservationInterface::GetUnits(Unit::Alliance alliance, Filter filter = {}) const {
+	return this->observations->GetUnits(alliance, filter);
+}
+
+Units SelfObservationInterface::GetUnits(Filter filter) const {
+	return this->observations->GetUnits(filter);
+}
+
+const Unit * SelfObservationInterface::GetUnit(Tag tag) const {
+	return this->observations->GetUnit(tag);
+}
+
+const RawActions & SelfObservationInterface::GetRawActions() const {
+	return this->observations->GetRawActions();
+}
+
+const SpatialActions & SelfObservationInterface::GetFeatureLayerActions() const {
+	return this->observations->GetFeatureLayerActions();
+}
+
+const SpatialActions & SelfObservationInterface::GetRenderedActions() const {
+	return this->observations->GetRenderedActions();
+}
+
+const std::vector< ChatMessage > & SelfObservationInterface::GetChatMessages() const {
+	return this->observations->GetChatMessages();
+}
+
+const std::vector< PowerSource > & SelfObservationInterface::GetPowerSources() const {
+	return this->observations->GetPowerSources();
+}
+
+const std::vector< Effect > & SelfObservationInterface::GetEffects() const {
+	return this->observations->GetEffects();
+}
+
+const std::vector< UpgradeID > & SelfObservationInterface::GetUpgrades() const {
+	return this->observations->GetUpgrades();
+}
+
+const Score & SelfObservationInterface::GetScore() const {
+	return this->observations->GetScore();
+}
+
+const Abilities & SelfObservationInterface::GetAbilityData(bool force_refresh = false) const {
+	return this->observations->GetAbilityData(force_refresh);
+}
+
+const UnitTypes & SelfObservationInterface::GetUnitTypeData(bool force_refresh = false) const {
+	return this->observations->GetUnitTypeData(force_refresh);
+}
+
+const Upgrades & SelfObservationInterface::GetUpgradeData(bool force_refresh = false) const {
+	return this->observations->GetUpgradeData(force_refresh);
+}
+
+const Buffs & SelfObservationInterface::GetBuffData(bool force_refresh = false) const {
+	return this->observations->GetBuffData(force_refresh);
+}
+
+const Effects & SelfObservationInterface::GetEffectData(bool force_refresh = false) const {
+	return this->observations->GetEffectData(force_refresh);
+}
+
+const GameInfo & SelfObservationInterface::GetGameInfo() const {
+	return this->observations->GetGameInfo();
+}
+
+int32_t SelfObservationInterface::GetMinerals() const {
+	return this->observations->GetMinerals();
+}
+
+int32_t SelfObservationInterface::GetVespene() const {
+	return this->observations->GetVespene();
+}
+
+int32_t SelfObservationInterface::GetFoodCap() const {
+	return this->observations->GetFoodCap();
+}
+
+int32_t SelfObservationInterface::GetFoodUsed() const {
+	return this->observations->GetFoodUsed();
+}
+
+int32_t SelfObservationInterface::GetFoodArmy() const {
+	return this->observations->GetFoodArmy();
+}
+
+int32_t SelfObservationInterface::GetFoodWorkers() const {
+	return this->observations->GetFoodWorkers();
+}
+
+int32_t SelfObservationInterface::GetIdleWorkerCount() const {
+	return this->observations->GetIdleWorkerCount();
+}
+
+int32_t SelfObservationInterface::GetArmyCount() const {
+	return this->observations->GetArmyCount();
+}
+
+int32_t SelfObservationInterface::GetWarpGateCount() const {
+	return this->observations->GetWarpGateCount();
+}
+
+Point2D SelfObservationInterface::GetCameraPos() const {
+	return this->observations->GetCameraPos();
+}
+
+Point3D SelfObservationInterface::GetStartLocation() const {
+	return this->observations->GetStartLocation();
+}
+
+const std::vector< PlayerResult > & SelfObservationInterface::GetResults() const {
+	return this->observations->GetResults();
+}
+
+bool SelfObservationInterface::HasCreep(const Point2D &point) const {
+	return this->observations->HasCreep(point);
+}
+
+Visibility SelfObservationInterface::GetVisibility(const Point2D &point) const {
+	return this->observations->GetVisibility(point);
+}
+
+bool SelfObservationInterface::IsPathable(const Point2D &point) const {
+	return this->observations->IsPathable(point);
+}
+
+bool SelfObservationInterface::IsPlacable(const Point2D &point) const {
+	return this->observations->IsPlacable(point);
+}
+
+float SelfObservationInterface::TerrainHeight(const Point2D &point) const {
+	return this->observations->TerrainHeight(point);
+}
---
+++ SelfObservationInterface.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <sc2api\sc2_interfaces.h>
+#include "BotStrategy.h"
+
+using namespace sc2;
+
+class SelfObservationInterface
+{
+public:
+	SelfObservationInterface(Unit* self, ObservationInterface* observations);
+	~SelfObservationInterface();
+
+	uint32_t GetPlayerID() const;
+	uint32_t SelfObservationInterface::GetGameLoop() const;
+	Units SelfObservationInterface::GetUnits() const;
+	Units SelfObservationInterface::GetUnits(Unit::Alliance alliance, Filter filter = {}) const;
+	Units SelfObservationInterface::GetUnits(Filter filter) const;
+	const Unit * SelfObservationInterface::GetUnit(Tag tag) const;
+	const RawActions & SelfObservationInterface::GetRawActions() const;
+	const SpatialActions & SelfObservationInterface::GetFeatureLayerActions() const;
+	const SpatialActions & SelfObservationInterface::GetRenderedActions() const;
+	const std::vector< ChatMessage > & SelfObservationInterface::GetChatMessages() const;
+	const std::vector< PowerSource > & SelfObservationInterface::GetPowerSources() const;
+	const std::vector< Effect > & SelfObservationInterface::GetEffects() const;
+	const std::vector< UpgradeID > & SelfObservationInterface::GetUpgrades() const;
+	const Score & SelfObservationInterface::GetScore() const;
+	const Abilities & SelfObservationInterface::GetAbilityData(bool force_refresh = false) const;
+	const UnitTypes & SelfObservationInterface::GetUnitTypeData(bool force_refresh = false) const;
+	const Upgrades & SelfObservationInterface::GetUpgradeData(bool force_refresh = false) const;
+	const Buffs & SelfObservationInterface::GetBuffData(bool force_refresh = false) const;
+	const Effects & SelfObservationInterface::GetEffectData(bool force_refresh = false) const;
+	const GameInfo & SelfObservationInterface::GetGameInfo() const;
+	int32_t SelfObservationInterface::GetMinerals() const;
+	int32_t SelfObservationInterface::GetVespene() const;
+	int32_t SelfObservationInterface::GetFoodCap() const;
+	int32_t SelfObservationInterface::GetFoodUsed() const;
+	int32_t SelfObservationInterface::GetFoodArmy() const;
+	int32_t SelfObservationInterface::GetFoodWorkers() const;
+	int32_t SelfObservationInterface::GetIdleWorkerCount() const;
+	int32_t SelfObservationInterface::GetArmyCount() const;
+	int32_t SelfObservationInterface::GetWarpGateCount() const;
+	Point2D SelfObservationInterface::GetCameraPos() const;
+	Point3D SelfObservationInterface::GetStartLocation() const;
+	const std::vector< PlayerResult > & SelfObservationInterface::GetResults() const;
+	bool SelfObservationInterface::HasCreep(const Point2D &point) const;
+	Visibility SelfObservationInterface::GetVisibility(const Point2D &point) const;
+	bool SelfObservationInterface::IsPathable(const Point2D &point) const;
+	bool SelfObservationInterface::IsPlacable(const Point2D &point) const;
+	float SelfObservationInterface::TerrainHeight(const Point2D &point) const;
+
+private:
+	Unit* self;
+	ObservationInterface* observations;
+	BotStrategy* strategy;
+};
+
