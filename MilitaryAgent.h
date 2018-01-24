b6dd0c9216de7adeaf893bd607fa71fedd3751e5
Aurélien POUGET <aurelien.pouget@eisti.fr>
2018-01-24 15:26:29 +0100

    Military Agent, déplacement des marines

--- MilitaryAgent.h
+++ MilitaryAgent.h
@@ -14,6 +14,8 @@ public:
 	void OnStep();
 	void OnUnitIdle();
 	void move();
+	Point2D findPlacement(ABILITY_ID abilityId, Point2D point, float increment = 0);
+	void RetreatWithUnit(const Unit* unit, Point2D retreat_position);
 	MilitaryAgent(const Unit* self, SelfActionInterface* actions, SelfObservationInterface* observations,QueryInterface* query);
 	~MilitaryAgent();
 };
